6bb4f587e883f72de32fe397c22f2e9236fad0de
MaxwellKwesiEhiawey <72410727+MaxwellKwesiEhiawey@users.noreply.github.com>
2020-11-05 18:21:50 +0000

    Maxwell_ShoppingCart

---
+++ Maxwell_ShoppinCart_201105/scripts/sales.js
@@ -0,0 +1,192 @@
+class Items {
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
+class ElementAttribute {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+
+class Component {
+  constructor(renderHookId, shouldRender = true) {
+    this.hookId = renderHookId;
+    if (shouldRender) {
+      this.render();
+    }
+  }
+
+  render() {}
+
+  createRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+      }
+    }
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
+}
+
+class ShoppingCart extends Component {
+  items = [];
+
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
+      2
+    )}</h2>`;
+  }
+
+  get totalAmount() {
+    const sum = this.items.reduce(
+      (prevValue, curItem) => prevValue + curItem.price,
+      0
+    );
+    return sum;
+  }
+
+  constructor(renderHookId) {
+    super(renderHookId, false);
+    this.orderProducts = () => {
+      console.log('Ordering...');
+      console.log(this.items);
+    };
+    this.render();
+  }
+
+  addItems(product) {
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
+  }
+
+  render() {
+    const cartEl = this.createRootElement('section', 'cart');
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
+    const orderButton = cartEl.querySelector('button');
+    orderButton.addEventListener('click', this.orderProducts);
+    this.totalOutput = cartEl.querySelector('h2');
+  }
+}
+
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId, false);
+    this.product = product;
+    this.render();
+  }
+
+  addToCart() {
+    App.addItemsToCart(this.product);
+  }
+
+  render() {
+    const prodEl = this.createRootElement('li', 'product-item');
+    prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+    const addCartButton = prodEl.querySelector('button');
+    addCartButton.addEventListener('click', this.addToCart.bind(this));
+  }
+}
+
+class ProductList extends Component {
+  #products = [];
+
+  constructor(renderHookId) {
+    super(renderHookId, false);
+    this.render();
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
+    this.#products = [
+      new Items(
+        'Luxurious Car',
+        'https://tricklearn.com/wp-content/uploads/2019/10/Expensive-car.jpg',
+        'Most Luxurious Car You Would Love!',
+        99.89 + ' M'
+      ),
+      new Items(
+        'Christmas Hamper',
+        'https://www.alyaka.com/magazine/wp-content/uploads/2014/12/hamper-feature.jpg',
+        'Christmas Hamper You cannot resist.',
+        29.76
+      ),
+      new Items ( 'Affordable Homes', 'https://www.agentimage.com/wp-content/uploads/2020/01/banner-photo-30-best-white-min.png', 'Cool Priced Homes', 3599.89
+      ),
+      new Items( 'Interior Decor Package', 'https://cdn.decorilla.com/online-decorating/wp-content/uploads/2018/03/hygge-home-decor-natural-light-1024x683.jpg', 'Interior Decor Package', 679.89
+       ),
+       new Items(
+         'Complete IT Equipments & Accesories', 'https://1.bp.blogspot.com/-5dhQCx47ZRc/W6DS0H3VZtI/AAAAAAAAJgw/wBVfJGxH0qo6QhXdLQ9SSICL_bVdDZGAgCLcBGAs/s640/Computer-hardware-components.jpg',
+         'Quality IT Equipments & Accesories Set',289.49, 
+       )
+    ];
+    this.renderProducts();
+  }
+
+  renderProducts() {
+    for (const prod of this.#products) {
+      new ProductItem(prod, 'prod-list');
+    }
+  }
+
+  render() {
+    this.createRootElement('ul', 'product-list', [
+      new ElementAttribute('id', 'prod-list')
+    ]);
+    if (this.#products && this.#products.length > 0) {
+      this.renderProducts();
+    }
+  }
+}
+
+class Shop {
+  constructor() {
+    this.render();
+  }
+
+  render() {
+    this.cart = new ShoppingCart('sales');
+    new ProductList('sales');
+  }
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+  
+}
+
+App.init();
